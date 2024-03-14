de6c59c1420715502220f57efb11edc22c8dbc2c
Alistair Abao <abaolester@gmail.com>
2024-03-14 15:14:44 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+//instantiate express
+const app = express();
+
+//parsing json
+app.use(express.json());
+
+//page not found
+app.use("*", (req, res, next) => {
+  res.status(404).json({ message: "Page not found" });
+});
+
+//express error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 400;
+  const message = err.message || "Something went wrong";
+  res.status(status).json({ message: message });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`SERVING PORT ${process.env.PORT}`);
+});
